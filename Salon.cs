0be5c20cc25f3d795da432cd69930110163804dc
AlejoIzzo <izzoalejo@gmail.com>
2024-06-16 23:13:33 -0300

    Create Salon.cs

---
+++ Salon.cs
@@ -0,0 +1,37 @@
+
+using System;
+using System.Collections;
+
+namespace proyecto2
+{
+	/// <summary>
+	/// Description of Salon.
+	/// </summary>
+	public class Salon
+	{
+		private string nombre;
+		private ArrayList listaEventos;
+		private ArrayList listaEmpleados;		
+		
+		public Salon(string nombre)
+		{
+			this.nombre = nombre;
+			listaEventos = new ArrayList();
+			listaEmpleados = new ArrayList();
+		}
+		
+		public void verAtributos(){
+			Console.WriteLine("Informacion de los eventos contratados:\n");
+			
+			foreach(Evento eve in listaEventos){
+				eve.verAtributos();
+			}
+			
+			Console.WriteLine("Informacion de los empleados del salon:\n");
+			foreach(Empleado emp in listaEmpleados){
+				emp.verAtributos();
+			}
+			
+		}
+	}
+}
